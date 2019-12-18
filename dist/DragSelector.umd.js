36731bd3f179bee9076ef81b8af42e20b948a54f
MikaelTroc <mikael@donnabeauty.se>
2019-12-18 14:45:37 +0100

    fix8

--- dist/DragSelector.umd.js
+++ dist/DragSelector.umd.js
@@ -1469,12 +1469,12 @@ if (typeof window !== 'undefined') {
 // EXTERNAL MODULE: ./node_modules/core-js/modules/es6.function.name.js
 var es6_function_name = __webpack_require__("7f7f");
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules//.cache//vue-loader","cacheIdentifier":"14310da3-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/DragSelector.vue?vue&type=template&id=6eaa4b36&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules//.cache//vue-loader","cacheIdentifier":"14310da3-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/DragSelector.vue?vue&type=template&id=78c3d356&
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"drag-selector-wrapper",on:{"mousedown":_vm.handleMouseDown}},[_c('div',{staticClass:"drag-area-box",style:(_vm.selectAreaStyle)}),_vm._t("default")],2)}
 var staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/DragSelector.vue?vue&type=template&id=6eaa4b36&
+// CONCATENATED MODULE: ./src/DragSelector.vue?vue&type=template&id=78c3d356&
 
 // EXTERNAL MODULE: ./node_modules/core-js/modules/web.dom.iterable.js
 var web_dom_iterable = __webpack_require__("ac6a");
@@ -1598,7 +1598,7 @@ var POINT_MODEL = ['offsetX', 'offsetY', 'clientX', 'clientY'];
   },
   mounted: function mounted() {
     this.handleMouseMoveThrottled = throttle(this.handleMouseMove);
-    this.emitChangeThrottled = throttle(this.emitChange, 50);
+    this.emitChangeThrottled = throttle(this.emitChange, 500);
   },
   methods: {
     registerEvents: function registerEvents() {
